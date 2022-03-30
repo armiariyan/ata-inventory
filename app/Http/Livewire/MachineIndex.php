1481bd961f72710b413f331dc2713aa959820f1f
Armia Riyan <akunarmia@gmail.com>
2022-03-30 15:57:37 +0700

    Show data in table for Machine

--- app/Http/Livewire/MachineIndex.php
+++ app/Http/Livewire/MachineIndex.php
@@ -3,11 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Machine;
 
 class MachineIndex extends Component
 {
     public function render()
     {
-        return view('livewire.machine-index');
+        return view('livewire.machine-index', [
+            'machines' => Machine::all()
+        ]);
+            
+
     }
+
 }
--- resources/views/livewire/machine-index.blade.php
+++ resources/views/livewire/machine-index.blade.php
@@ -1,4 +1,28 @@
 <div>
     {{-- Do your work, then step back. --}}
-    <h1>Hellow World</h1>
+    <table class="table table-striped table-hover table-bordered">
+        <thead class="">
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Data Notes</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($machines as $machine)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $machine->name }}</td>
+                    <td>{{ $machine->type }}</td>
+                    <td>{{ $machine->notes }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-info text-white">Edit</button>
+                        <button class="btn btn-sm btn-danger text-white" onclick="alert('Are you sure want to delete this data?')">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody> 
+    </table>
 </div>
